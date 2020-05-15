33344f1ca61a50c35d1aad0a8d40ff042772d5dc
skundu <shiladitya.kundu@capgemini.com>
2020-05-15 18:12:12 +0530

    initial commit

---
+++ src/test/java/com/example/addon/AddonMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.addon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddonMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
